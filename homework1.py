2cf0eebbbb649437094755326a4a3104e3906a08
Lera <99478998+lerizlot@users.noreply.github.com>
2023-04-10 09:50:48 +0400

    all tasks have been completed

--- homework1.py
+++ homework1.py
@@ -1,4 +1,5 @@
-# Задача 2: Найдите сумму цифр трехзначного числа.
+# Задача 2:
+# Найдите сумму цифр трехзначного числа.
 
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
@@ -14,45 +15,46 @@ while num > 0:
 
 print("The sum of the digits is:", sum)
 
-# Задача 4: Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов. Сколько журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов, а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?
+# Задача 4: 
+# Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов. Сколько журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов, а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?
 
 # *Пример:*
 # 6 -> 1  4  1
 # 24 -> 4  16  4
 # 60 -> 10  40  10
 
-# Ask the user for the total number of cranes
 S = int(input("Enter the total number of cranes: "))
+x = S // 6
 
-x = S // 4
-
-# Calculate the number of cranes made by Petya and Seryozha
 petya = x
 seryozha = x
 
-# Calculate the number of cranes made by Katya
 katya = (petya + seryozha) * 2
 
 print("Petya made", petya, "cranes.")
 print("Katya made", katya, "cranes.")
 print("Seryozha made", seryozha, "cranes.")
 
-# Задача 6: Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером. Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех. Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
+# Задача 6:
+# Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером. Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех. Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
 
 # *Пример:*
 # 385916 -> yes
 # 123456 -> no
 
-ticket_num = input("Enter the ticket number: ")
+num = input("Enter a 6-digit number: ")
 
-# Split the ticket number into individual digits and convert them to integers
-digits = [int(d) for d in ticket_num]
-
-# Check if the sum of the first three digits is equal to the sum of the last three digits
-if sum(digits[:3]) == sum(digits[3:]):
-    print("The ticket number", ticket_num, "is lucky!")
+# Check if the input has exactly 6 digits
+if len(num) != 6:
+    print("Error: Input must have exactly 6 digits")
 else:
-    print("The ticket number", ticket_num, "is not lucky.")
+    first_three = int(num[0]) + int(num[1]) + int(num[2])
+    last_three = int(num[3]) + int(num[4]) + int(num[5])
+    
+    if first_three == last_three:
+        print("The ticket is lucky")
+    else:
+        print("The ticket is not lucky")
 
 # Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
 
